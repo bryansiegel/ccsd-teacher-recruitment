2e4e965aa562bc67d66167c2750d138c630db3e3
Bryan Siegel <bryansiegel@gmail.com>
2025-03-17 09:28:55 -0700

    code refactor

--- index.php
+++ index.php
@@ -9,41 +9,32 @@
     <div class="row">
         <div class="col">
 
-<table id="recruitmentTable" class="table table-striped table-bordered">
-    <thead>
-    <tr>
-        <th>Title</th>
-        <th>Content</th>
-        <th>Published</th>
-        <th></th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php /** @var TYPE_NAME $rss */
-foreach ($rss as $item) { ?>
-    <?php if(!empty($item->published)) { ?>
-    <tr>
-        <td><a href='<?php echo $item->link['href']; ?>'><?php echo $item->title; ?></a></td>
-
-        <td><?php echo $item->content ?></td>
-
-
-            <?php
-
-            $pubDate = date("F j, Y, g:i a", strtotime($item->published));
-//            var_dump($pubDate);
-
-            ?>
-        <td><?php echo $pubDate ?></td>
-            <td><a href="" class="btn btn-primary">Save</a></td>
-
-    </tr>
-    <?php } ?>
-     <?php } ?>
-    </tbody>
+            <table id="recruitmentTable" class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Published</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php /** @var TYPE_NAME $rss */
+                foreach ($rss as $item) { ?>
+                    <?php if (!empty($item->published)) { ?>
+                        <tr>
+                            <td><a href='<?php echo $item->link['href']; ?>'><?php echo $item->title; ?></a></td>
+                            <td><?php echo $item->content ?></td>
+                            <?php $pubDate = date("F j, Y, g:i a", strtotime($item->published)); ?>
+                            <td><?php echo $pubDate ?></td>
+                            <td><a href="" class="btn btn-primary">Save</a></td>
+                        </tr>
+                    <?php }
+                } ?>
+                </tbody>
 
 
-</table>
+            </table>
         </div>
     </div>
 </div> <!-- /.container -->
