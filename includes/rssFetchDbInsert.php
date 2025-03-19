fdf8e8977872afe44aa32aaa0e6d29f4b11e00d0
Bryan Siegel <bryansiegel@gmail.com>
2025-03-19 11:16:28 -0700

    added active and saved to db

--- includes/rssFetchDbInsert.php
+++ includes/rssFetchDbInsert.php
@@ -3,11 +3,21 @@
 $db = new SQLite3('teacherRecruitment.db');
 
 // Create table if it doesn't exist
-$db->exec("CREATE TABLE IF NOT EXISTS teacherRecruitment (id INTEGER PRIMARY KEY, title TEXT UNIQUE, content TEXT UNIQUE, published TEXT, link TEXT UNIQUE)");
+$db->exec("CREATE TABLE IF NOT EXISTS teacherRecruitment (id INTEGER PRIMARY KEY, title TEXT UNIQUE, content TEXT UNIQUE, published TEXT, link TEXT UNIQUE,
+    active BOOLEAN DEFAULT 1, 
+    saved BOOLEAN DEFAULT 0)");
 
 $rss_feeds = [
+    //teacher layoffs
     "https://www.google.com/alerts/feeds/09799090004667660386/14564746603821698168",
-    "https://www.google.com/alerts/feeds/09799090004667660386/16541197399518915531"
+    //teacher reduction in force
+    "https://www.google.com/alerts/feeds/09799090004667660386/16541197399518915531",
+    //teacher layoffs california
+    "https://www.google.com/alerts/feeds/09799090004667660386/15986459700403287516",
+    //low teacher pay
+    "https://www.google.com/alerts/feeds/09799090004667660386/7381414631943293210",
+    //boston teacher layoffs
+    "https://www.google.com/alerts/feeds/09799090004667660386/13008520046193247680"
 ];
 
 foreach ($rss_feeds as $rss_feed) {
@@ -22,14 +32,19 @@ foreach ($rss_feeds as $rss_feed) {
         if (!empty($item->published)) {
             $pubDate = date("F j, Y, g:i a", strtotime($item->published));
 
-            // Prepare the insert statement
-            $stmt = $db->prepare("INSERT OR IGNORE INTO teacherRecruitment (title, content, published, link) VALUES (:title, :content, :published, :link)");
+            // Prepare the insert statement to sqlite
+            $stmt = $db->prepare("INSERT OR IGNORE INTO teacherRecruitment (title, content, published, link, active, saved) VALUES (:title, :content, :published, :link, 1,0)");
 
+//            $active = 1;
+//            $saved = 0;
             // Bind parameters
             $stmt->bindParam(':title', $item->title, SQLITE3_TEXT);
             $stmt->bindParam(':content', $item->content, SQLITE3_TEXT);
             $stmt->bindParam(':published', $pubDate, SQLITE3_TEXT);
             $stmt->bindParam(':link', $item->link['href'], SQLITE3_TEXT);
+            $stmt->bindParam(':active', $item->link, SQLITE3_INTEGER);
+            $stmt->bindParam(':saved', $item->saved, SQLITE3_TEXT);
+
 
             // Execute the statement
             $stmt->execute();
